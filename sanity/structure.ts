4a8232b78e0ea59bc13692b2acb6460c9a672cfa
reyesprince31 <reyes.prince31@gmail.com>
2024-11-09 16:24:43 +0800

    product and category schemaType

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,15 +1,16 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from 'sanity/structure'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title('ATVS Ecommerce')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
       S.documentTypeListItem('category').title('Categories'),
+      S.divider(),
       S.documentTypeListItem('author').title('Authors'),
+      S.documentTypeListItem('product').title('Products'),
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
+        (item) => item.getId() && !['post', 'category', 'author', 'product'].includes(item.getId()!),
       ),
     ])
